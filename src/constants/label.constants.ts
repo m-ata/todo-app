48b19b0ccbaeeb6a10592c4fa62fa953bfea6201
Muhammad Ata <muhammad_ata@outlook.com>
2023-09-10 00:46:41 +0200

    style: styled page not found

---
+++ src/constants/label.constants.ts
@@ -0,0 +1,3 @@
+export const PAGE_NOT_FOUND = "OOPS! PAGE NOT FOUND";
+export const NOT_FOUND_DESCRIPTION = 'The page you are looking for was moved, removed, renamed or might never existed.';
+export const GO_HOME_BUTTON = 'GO HOME';
