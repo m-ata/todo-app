ea4ec23f11762a87b01f77fcb485d3866c2954f3
Muhammad Ata <muhammad_ata@outlook.com>
2023-09-10 17:51:27 +0200

    chore: code commenting in todo, not-found page

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,24 +1,32 @@
-import Header from '@components/Header';
-import TodoList from '@components/TodoList';
-import './styles.scss';
-import { useGetTodosQuery } from '@/redux/services/todo.service';
+// imports from react
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setTodo } from '@redux/slices/todo.slice';
-import { ITodo, RTKQueryResponse } from '@/interfaces/todo.interface';
 import { toast } from 'react-toastify';
-import { getApiError } from '@/utils/apiError.utils';
+// import components
+import Header from '@components/Header';
+import TodoList from '@components/TodoList';
+//imports from redux
+import { useGetTodosQuery } from '@redux/services/todo.service';
+import { setTodo } from '@redux/slices/todo.slice';
+// import interfaces
+import { ITodo, RTKQueryResponse } from '@/interfaces/todo.interface';
+// import util
+import { getApiError } from '@utils/apiError.utils';
+// import style
+import './styles.scss';
 
 const Todo = () => {
+  // api call using RTK Query to get todos
   const { data, error } = useGetTodosQuery() as RTKQueryResponse;
+
   const dispatch = useDispatch();
-  console.log(data, error);
-  // setting todo in store on componentDidUpdate
+
+  // componentDidUpdate (data, error)
   useEffect(() => {
-    if (data)
-    dispatch(setTodo(data as ITodo[]));
+    if (data) dispatch(setTodo(data as ITodo[])); // setting todo in store
+    // showing error in toast
     if (error) toast.error(getApiError(error));
-  }, [data]);
+  }, [data, error]);
 
   return (
     <div className="container">
