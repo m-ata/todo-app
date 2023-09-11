67e257be67d29b6c205c60bc9c4ee1d54738883e
Muhammad Ata <muhammad_ata@outlook.com>
2023-09-11 23:44:03 +0200

    chore: shift all label constants into label.constant.ts

--- src/constants/label.constants.ts
+++ src/constants/label.constants.ts
@@ -1,5 +1,6 @@
-export const PAGE_NOT_FOUND = "OOPS! PAGE NOT FOUND";
-export const NOT_FOUND_DESCRIPTION = 'The page you are looking for was moved, removed, renamed or might never existed.';
+export const PAGE_NOT_FOUND = 'OOPS! PAGE NOT FOUND';
+export const NOT_FOUND_DESCRIPTION =
+  'The page you are looking for was moved, removed, renamed or might never existed.';
 export const GO_HOME_BUTTON = 'GO HOME';
 
 export const ITEMS_PER_PAGE = 'Items per page';
@@ -7,3 +8,15 @@ export const TASK = 'Task';
 export const DEADLINE = 'Deadline';
 export const ADD_TODO = 'Add Todo';
 export const EDIT_TODO = 'Edit Todo';
+
+export const DELETE_TODO_CONFIRMATION_HEADING = 'Delete Task';
+export const DELETE_TODO_CONFIRMATION_CONTENT =
+  'Are you sure, you want to delete this task?';
+
+export const COMPLETE_TODO_CONFIRMATION_HEADING = 'Complete Task';
+export const COMPLETE_TODO_CONFIRMATION_CONTENT =
+  'Are you sure, this task is completed?';
+
+export const DATA_NOT_FOUND = 'Data Not Found';
+
+export const TODO_APP_LABEL = 'Todo App';
